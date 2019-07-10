d317a19c458352a8fa21b7429a93d81e7e40a6df
handsinoil <handsinoil@gmail.com>
2019-07-10 18:38:42 +0400

    готов к проверке

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
 class BasePageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
