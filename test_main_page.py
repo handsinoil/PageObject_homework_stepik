9c7874fcf0a52fedde108e0333f50adfa8af794a
handsinoil <handsinoil@gmail.com>
2019-07-10 18:43:54 +0400

    готов к проверке

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,6 @@ import pytest
 
 link = "http://selenium1py.pythonanywhere.com"
 
-
 @pytest.mark.login_guest
 class TestLoginFromMainPage(object):
     def test_guest_can_go_to_login_page(self, browser):
